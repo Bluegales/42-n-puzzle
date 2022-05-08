eaba3e5c048ff132a62b798b26af847e487b9c03
pfuchs <pfuchs@student.42heilbronn.de>
2022-05-09 01:11:42 +0200

    fixed css

--- pain.sh
+++ pain.sh
@@ -2,6 +2,6 @@
 python3 npuzzle-gen.py 4 > ./files/gen.txt
 #./solver/test ./files/npuzzle-4-1.txt
 #valgrind ./solver/test ./files/npuzzle-3-1.txt
-#./solver/test ./files/gen.txt
-./solver/test ./files/simple.txt
+./solver/test ./files/gen.txt
+#./solver/test ./files/simple.txt
 cat ./files/gen.txt | head -1
